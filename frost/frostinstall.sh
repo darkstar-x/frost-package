ab06810a6f28b23eff37795477de1e0f21a9d948
hxvrmxnv <leonardigit@gmail.com>
2022-10-03 19:45:36 -0300

    fix bug on frostinstall

--- frost/frostinstall.sh
+++ frost/frostinstall.sh
@@ -12,8 +12,25 @@ TRACK='/var/log/frost'
 #creating the tracker directory
 [ ! -d "$TRACK" ] && mkdir -v "$TRACK"
 
+### Functions
+INSTALL()
+{
+  local package="$1" #package name
+
+  printf '%b' "||||| 0% initialize package instalation: $package\n"
+
+  tar xvf "$package" -C /ssf/virtual
+}
+
+
 case $1 in
   -i|--install)
       shift
+      if [ -e "$1" ]; then
+        INSTALL $1
+      else
+        echo "Package $1 is not exitent"
+        exit 1
+      fi
     ;;
 esac
